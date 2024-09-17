6f896ed2430f56ec401c052fef837d98335d1f55
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-09-17 13:19:55 +0200

    install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, isDevMode } from "@angular/core";
 import {
   BrowserModule,
   provideClientHydration,
@@ -8,10 +8,26 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { SideBarComponent } from "./components/side-bar/side-bar.component";
+import { StoreModule } from "@ngrx/store";
+import { counterReducer } from "./store/tasks/tasks.reducers";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { UIReducer } from "./store/ui/ui.reducers";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, SideBarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ count: counterReducer, ui: UIReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true, // If set to true, the connection is established within the Angular zone
+    }),
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
