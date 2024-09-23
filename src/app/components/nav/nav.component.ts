4f2fef66605daf84d777a6666aba0c424ad73025
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-09-23 12:09:29 +0200

    quickFix add new board

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -4,6 +4,8 @@ import { AppState } from "../../models/state/app.state.interface";
 import * as UIActions from "../../store/ui/ui.actions";
 import { Observable } from "rxjs";
 import { selectSideBarToggler } from "../../store/ui/ui.selectors";
+import { Board } from "../../models/data/board.interface";
+import { selectActiveBoard } from "../../store/tasks/tasks.selectors";
 
 @Component({
   selector: "app-nav",
@@ -11,9 +13,11 @@ import { selectSideBarToggler } from "../../store/ui/ui.selectors";
   styleUrl: "./nav.component.scss",
 })
 export class NavComponent {
+  activeBoard$!: Observable<Board>;
   toggleSideBar$!: Observable<boolean>;
   constructor(private store: Store<AppState>) {
     this.toggleSideBar$ = this.store.pipe(select(selectSideBarToggler));
+    this.activeBoard$ = this.store.pipe(select(selectActiveBoard));
   }
   toggleSideBar() {
     this.store.dispatch(UIActions.toggleSideBar());
